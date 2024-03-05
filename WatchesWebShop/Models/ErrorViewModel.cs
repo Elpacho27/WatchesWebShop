4e152da68e01e10a748348c40b3093c343b87c19
Elpacho27 <patrik27ocelic03@gmail.com>
2024-03-05 22:50:13 +0100

    Add project files.

---
+++ WatchesWebShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatchesWebShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
